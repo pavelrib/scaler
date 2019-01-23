cecba7c65f6f52aa2613e24226fc86d25e3b47a6
Pavel <pavelr3@gmail.com>
2019-01-23 18:23:02 +0200

    Rewriting initial

--- pkg/resourcescaler/resourcescaler.go
+++ pkg/resourcescaler/resourcescaler.go
@@ -2,11 +2,13 @@ package resourcescaler
 
 import (
 	"fmt"
+
 	"github.com/nuclio/errors"
 	"github.com/nuclio/logger"
 	"github.com/v3io/scaler/pkg"
 	"k8s.io/helm/pkg/helm"
 	hapi_chart3 "k8s.io/helm/pkg/proto/hapi/chart"
+	"k8s.io/helm/pkg/proto/hapi/release"
 )
 
 type AppResourceScaler struct {
@@ -30,118 +32,86 @@ func New() scaler.ResourceScaler {
 // if last int parameter is 0 -> helm del --purge. if 1 -> helm install
 func (r *AppResourceScaler) SetScale(logger logger.Logger, namespace string, resource scaler.Resource, scaling int) error {
 	if scaling == 0 {
-		if err := r.saveResourceData(logger, resource); err != nil {
-			return errors.Wrap(err, "Failed setting resource")
-		}
 
+		// delete resource, but don't purge it, for future reference
 		deleteResponse, err := r.helmClient.DeleteRelease(string(resource))
 		if err != nil {
 			return errors.Wrap(err, fmt.Sprintf("Failed to delete release %s", string(resource)))
 		}
-		logger.InfoWith("Release deleted successfully", "release_name", resource, "delete_response", deleteResponse)
+		logger.InfoWith("Release deleted successfully",
+			"release_name", resource, "delete_response", deleteResponse)
 	} else {
-		//TBD
+
+		// list deleted releases of resource name
+		listNamespace := helm.ReleaseListNamespace(namespace)
+		listDeleted := helm.ReleaseListStatuses([]release.Status_Code{release.Status_DELETED, release.Status_DELETING})
+		listName := helm.ReleaseListFilter(string(resource))
+		listedReleases, err := r.helmClient.ListReleases(listNamespace, listDeleted, listName)
+		if err != nil {
+			logger.WarnWith("Failed while retrieving deleted releases for resource",
+				"resource_name", string(resource))
+			return errors.Wrap(err,"Failed to list deleted releases for resource")
+		}
+
+		// there should be exactly one release with this name
+		releases := listedReleases.GetReleases()
+		if len(releases) == 0 {
+			logger.WarnWith("Could not find deleted releases for resource", "resource_name", string(resource))
+			return errors.New("No deleted releases found, nothing to scale up")
+		}
+
+		if len(releases) > 1 {
+			releasesNames := make([]string, 0)
+			for _, releaseInst := range releases {
+				releasesNames = append(releasesNames, releaseInst.Name)
+			}
+			logger.WarnWith("Find too many releases with same name", "resources_names", releasesNames)
+			return errors.New("Too many releases to scale")
+		}
+
+		// found a single release - get its chart name/version
+		releaseChart := releases[0].GetChart()
+		logger.DebugWith("Retrieved chart of a deleted release",
+			"chart_version", releaseChart.GetMetadata().GetName())
+
+		// run a helm install command
+		_, err = r.helmClient.InstallReleaseFromChart(releaseChart, namespace, helm.InstallReuseName(true))
+		if err != nil {
+			logger.WarnWith("Failed while re-installing release from chart", "release_name", string(resource))
+			return errors.Wrap(err, "Failed while reinstalling")
+		}
+		logger.InfoWith("Release was re-installed successfully", "release_name", string(resource))
 	}
 
 	return nil
 }
 
 func (r *AppResourceScaler) GetResources(namespace string) ([]scaler.Resource, error) {
-	r.helmClient.ListReleases()
-	return []scaler.Resource{"jupyter"}, nil
+	resources := make([]scaler.Resource, 0)
+
+	listNamespace := helm.ReleaseListNamespace(namespace)
+	allStatuses := helm.ReleaseListStatuses(allReleaseStatuses())
+	listedReleases, err := r.helmClient.ListReleases(listNamespace, allStatuses)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to get listed releases")
+	}
+
+	// return the names of all releases
+	for _, releaseInst := range listedReleases.GetReleases() {
+		resources = append(resources, scaler.Resource(releaseInst.Name))
+	}
+
+	return resources, nil
 }
 
 func (r *AppResourceScaler) GetConfig() (*scaler.ResourceScalerConfig, error) {
-
-	//// Autoscaler options definition
-	//scaleInterval, err := time.ParseDuration(os.Getenv("AUTOSCALER_SCALE_INTERVAL"))
-	//if err != nil {
-	//	return nil, errors.Wrap(err, "Failed to parse autoscaler scale interval")
-	//}
-	//
-	//scaleWindow, err := time.ParseDuration(os.Getenv("AUTOSCALER_SCALE_WINDOW"))
-	//if err != nil {
-	//	return nil, errors.Wrap(err, "Failed to parse autoscaler scale window")
-	//}
-	//
-	//threshold, err := strconv.Atoi(os.Getenv("AUTOSCALER_THRESHOLD"))
-	//if err != nil {
-	//	return nil, errors.Wrap(err, "Failed to parse autoscaler threshold")
-	//}
-	//
-	//autoscalerOptions := scaler.AutoScalerOptions{
-	//	Namespace:     r.namespace,
-	//	ScaleInterval: scaleInterval,
-	//	ScaleWindow:   scaleWindow,
-	//	MetricName:    os.Getenv("AUTOSCALER_METRIC_NAME"),
-	//	Threshold:     int64(threshold),
-	//}
-	//
-	//// Poller options definitions
-	//pollerMetricInterval, err := time.ParseDuration(os.Getenv("POLLER_METRIC_INTERVAL"))
-	//if err != nil {
-	//	return nil, errors.Wrap(err, "Failed to parse poller metric interval")
-	//}
-	//
-	//pollerOptions := scaler.PollerOptions{
-	//	MetricInterval: pollerMetricInterval,
-	//	MetricName:     os.Getenv("POLLER_METRIC_NAME"),
-	//	Namespace:      r.namespace,
-	//}
-	//
-	//// DLX options definition
-	//targetPort, err := strconv.Atoi(os.Getenv("DLX_TARGET_PORT"))
-	//if err != nil {
-	//	return nil, errors.Wrap(err, "Failed to parse dlx target port")
-	//}
-	//
-	//dlxOptions := scaler.DLXOptions{
-	//	Namespace:        r.namespace,
-	//	TargetNameHeader: os.Getenv("DLX_TARGET_NAME_HEADER"),
-	//	TargetPathHeader: os.Getenv("DLX_TARGET_PATH_HEADER"),
-	//	TargetPort:       targetPort,
-	//	ListenAddress:    os.Getenv("DLX_LISTEN_ADDRESS"),
-	//}
-	//
-	//// Now combine everything
-	//return &scaler.ResourceScalerConfig{
-	//	KubeconfigPath:    os.Getenv("KUBECONFIG_PATH"),
-	//	AutoScalerOptions: autoscalerOptions,
-	//	PollerOptions:     pollerOptions,
-	//	DLXOptions:        dlxOptions,
-	//}, nil
-
 	return nil, nil
 }
 
-//func getenv(key, fallback string) string {
-//	value := os.Getenv(key)
-//	if len(value) == 0 {
-//		return fallback
-//	}
-//	return value
-//}
-
-func (r *AppResourceScaler) saveResourceData(logger logger.Logger, resource scaler.Resource) error {
-	// clean previous data
-	r.releases = r.releases[:0]
-
-	// get required values
-	listNamespace := helm.ReleaseListNamespace(r.namespace)
-	listFilter := helm.ReleaseListFilter(string(resource))
-	listedReleases, err := r.helmClient.ListReleases(listNamespace, listFilter)
-
-	if err != nil {
-		return errors.Wrap(err, "Failed to get listed releases")
+func allReleaseStatuses() []release.Status_Code {
+	statusCodes := make([]release.Status_Code, 0)
+	for statusCodeValue := range release.Status_Code_name {
+		statusCodes = append(statusCodes, release.Status_Code(statusCodeValue))
 	}
-
-	for _, release := range listedReleases.GetReleases() {
-		logger.DebugWith("Saving release data for later use", "release_name", release.Name)
-		singleReleaseData := &releaseData{
-			chart: release.Chart,
-		}
-		r.releases = append(r.releases, singleReleaseData)
-	}
-
-	return nil
+	return statusCodes
 }
